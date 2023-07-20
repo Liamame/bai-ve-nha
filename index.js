e5bbff3b3c2feca0f665d72e98ad6366fe007774
Liamame <137408345+Liamame@users.noreply.github.com>
2023-07-20 18:35:52 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+// bài 1
+// function sumArray(mang) {
+//     let sum = 0;
+//     for (let i = 0; i < mang.length; i++) {
+//         sum += mang[i];
+//     }
+//     return sum;
+// }
+
+
+// bài 2
+// let mang = [1, 5, 9, 10];
+// console.log(sumArray(mang));
+// let num = [5, 4, 7, 2, 8, 7, 3];
+// let max_val = num.reduce(function (accumulator, element) {
+//     return (accumulator > element) ? accumulator : element
+// });
+// console.log(num);
+// console.log("max= ", max_val);
+
+
+// bài 3
+// let num = [5, 4, 7, 2, 8, 7, 3];
+// let max_val = num.reduce(function (accumulator, element) {
+//     return (accumulator < element) ? accumulator : element
+// });
+// console.log(num);
+// console.log("min= ", max_val);
+
+
+// bài 5
+// const originalArray = [1, 2, 3, 4, 5];
+// originalArray.reverse();
+// console.log(originalArray);
+
+
+// bài 6
+// const numbers = [7, 10, 15, 8, 13, 18, 6]; const evens = [];
+// numbers.forEach((num) => {
+//     if (num % 2 === 0) {
+//         evens.push(num);
+//     }
+// })
+// console.log(evens);
+
+
+// bài 8
+// var score = [40, 100, 1, 6, 35, 10];
+//     score.sort(function(a, b){return a - b});
